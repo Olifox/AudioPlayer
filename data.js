1f549db1d8cc8d98ca1a6d0c33b93acc320862bb
Olifox <39879425+Olifox@users.noreply.github.com>
2018-06-03 23:40:15 +0700

    Source for online audio player

---
+++ data.js
@@ -0,0 +1,46 @@
+recordsData = [  
+{id:"1", value:"Linkin Park", data: [
+    {id:"linkData", value:"One More Light"},
+    ]},
+{id:"2", value:"Devil sold his soul", data: [
+    {id:"devilData", value:"Empire of Light"},
+    ]},
+];
+
+gridColumns = [
+    {
+        dataIndex: "title",
+        header: "Title"
+    },
+    {
+        dataIndex: "duration",
+        header: "Duration"
+    }
+];
+
+linkData = [
+    {id: "1", title: "01. Nobody Can Save Me", duration:"3:45"},
+    {id: "2", title: "02. Good Goodbye", duration:"3:31"},
+    {id: "3", title: "03. Talking to Myself", duration:"3:51"},
+    {id: "4", title: "04. Battle Symphony", duration:"3:36"},
+    {id: "5", title: "05. Invisible", duration:"3:34"},
+    {id: "6", title: "06. Heavy [Explicit]", duration:"2:49"},
+    {id: "7", title: "07. Sorry for Now", duration:"3:23"},
+    {id: "8", title: "08. Halfway Right", duration:"3:37"},
+    {id: "9", title: "09. One More Light", duration:"4:15"},
+    {id: "10", title: "10. Sharp Edges", duration:"2:58"},
+];
+
+devilData = [
+    {id: "1", title: "01. No Remorse, No Regrets", duration: "4:51"},
+    {id: "2", title: "02. A New Legacy", duration: "3:39"},
+    {id: "3", title: "03. VIII", duration: "3:21"},
+    {id: "4", title: "04. It Rains Down", duration: "4:22"},
+    {id: "5", title: "05. The Waves and the Seas", duration: "5:51"},
+    {id: "6", title: "06. Sorrow Plagues", duration: "4:55"},
+    {id: "7", title: "07. Time and Pressure", duration: "4:59"},
+    {id: "8", title: "08. Salvation Lies Within", duration: "4:08"},
+    {id: "9", title: "09. Crusader", duration: "7:12"},
+    {id: "10", title: "10. The Verge", duration: "6:00"},
+    {id: "11", title: "11. End of Days", duration: "9:24"},
+]
